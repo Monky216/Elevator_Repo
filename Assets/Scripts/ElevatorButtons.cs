6e7c627964b033cb1a2393e203fefd8c41bffcdb
Monky216 <90779085+Monky216@users.noreply.github.com>
2022-03-22 10:03:42 -0400

    Setting up rooms and scripts
    Cubes be spinning and changing colors. Still debating on how scripts will be organized.

---
+++ Assets/Scripts/ElevatorButtons.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorButtons : MonoBehaviour
+{
+    public bool isPushed = false;
+    
+    void Update()
+    {
+        if (isPushed == true)
+        {
+            //if button is room A, B, or C (run that script)
+                //different scripts or new almighty script for variables
+            //set something to return to false (when doors open to new room)
+        }
+    }
+}
